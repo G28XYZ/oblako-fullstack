26cb46cc120c48716c5ca3436c4761db9e8e2e94
g28xyz <g28xyz@ya.ru>
2024-03-23 14:40:04 +0700

    удаление лишнего

--- backend/files/urls.py
+++ backend/files/urls.py
@@ -1,14 +1,3 @@
-# path('api/signin/', login_view),
-# path('api/signup/', RegistrUserView.as_view()),
-# path('api/logout/', logout_view),
-# path('api/auth/get_csrf/', get_csrf_token),
-# path('api/me/', me_view),
-# path('api/users/delete/<int:user_id>/', delete_user),
-# path('api/files/', FileView.as_view()),
-# path('api/detail_users_list/', get_detail_user_list),
-# path('api/link/', get_link),
-# path('api/link/<str:link>/', get_file),
-
 from django.urls import path
 
 from .views import FileAPIView, FileDeleteAPIView, get_link, download_file
