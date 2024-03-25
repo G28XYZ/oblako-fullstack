64b22fce8c59c8d836fb62e042e28830dbfe0bf0
g28xyz <g28xyz@ya.ru>
2024-03-25 15:55:31 +0700

    fix copy to clipboard and add readme

--- frontend/src/pages/storage/reducer.js
+++ frontend/src/pages/storage/reducer.js
@@ -95,7 +95,26 @@ export const storageSlice = createSlice({
                 state.errorSaveFile = "";
             })
             .addCase(onGetDownloadFileLink.fulfilled, (state, { payload }) => {
-                navigator.clipboard.writeText(`${BASE_URL}/api/files/download/${payload?.link_id}`);
+                const unsecuredCopyToClipboard = (text) => {
+                    const textArea = document.createElement("textarea");
+                    textArea.value = text;
+                    document.body.appendChild(textArea);
+                    textArea.focus();
+                    textArea.select();
+                    try {
+                        document.execCommand("copy");
+                    } catch (err) {
+                        console.error("Unable to copy to clipboard", err);
+                    }
+                    document.body.removeChild(textArea);
+                    textArea.remove();
+                };
+                const content = `${BASE_URL}/api/files/download/${payload?.link_id}`;
+                if (window.isSecureContext && navigator.clipboard) {
+                    navigator.clipboard.writeText(content);
+                } else {
+                    unsecuredCopyToClipboard(content);
+                }
             })
             .addCase(onDownloadFile.fulfilled, (state) => {
                 state.errorSaveFile = "";
